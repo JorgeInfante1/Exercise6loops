11883602cb3589079cd667c79d42faca2f18f464
JorgeInfante1 <jorge_infante90@hotmail.com>
2019-10-14 13:06:22 -0500

    ForLoops

--- src/com/exercise6loops/app/Loops.java
+++ src/com/exercise6loops/app/Loops.java
@@ -1,9 +1,44 @@
 package com.exercise6loops.app;
 
-public class Loops {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Loops 
+{
+
+	public static void main(String[] args) 
+	{
+		// Constant declaration
+		
+		// Variable declaration
+		
+		//Objects construction
+		
+		//Create a Series of 1-100 1 by 1
+		for (int i=100;i>=1;i--)
+		{
+			System.out.println(i);
+		}
+		
+		for(int i=0;i<=100;i+=2);
+		{
+			System.out.println(i);
+		}
+		for(int i=0;i<200;i++)
+		{
+			if(i%2==0)
+			{
+				continue;
+			}
+			System.out.println(i);
+		}
+		System.out.println("Break point ");
+		for(int i=100;i<200;i++)
+		{
+			System.out.println(i);
+			if(i==150)
+			{
+				break;
+			}
+		}
 
 	}
 
